28ed0103db3f2903c5b4ee5a9bf01ffccfa6d5c2
Daniel Bogan <d@waferbaby.com>
2013-09-25 23:16:39 -0700

    Initial import.

---
+++ lib/salt.rb
@@ -0,0 +1,11 @@
+$:.unshift(__dir__)
+
+require 'rubygems'
+require 'erubis'
+require 'yaml'
+
+require 'salt/frontable'
+require 'salt/page'
+require 'salt/post'
+require 'salt/site'
+require 'salt/template'
