95322bea9ac87eb640820f93204cfa44fc38be45
Daniel Bogan <d@waferbaby.com>
2013-09-28 12:04:13 -0700

    Made all the attr_accessors private. Made stuff start to actually work.

--- lib/salt/site.rb
+++ lib/salt/site.rb
@@ -1,13 +1,11 @@
 module Salt
   class Site
-    attr_accessor :paths, :templates, :pages, :posts
-
-    def self.instance()
-      @site ||= new
+    def self.instance
+      @instance ||= new
     end
 
-    def initialize()
-      @paths, @templates, @pages, @posts = {}, {}, [], []
+    def initialize
+      @paths, @templates, @settings, @pages, @posts, @error = {}, {}, {}, [], [], nil
 
       @klasses = {
         page: Salt::Page,
@@ -25,15 +23,21 @@ module Salt
     end
 
     def setup(config = {})
-      @paths[:source] = File.expand_path(config[:source]) || Dir.pwd
+      @paths[:source] = File.expand_path(config[:source] || Dir.pwd)
 
       %w{pages posts templates site}.each do |path|
         path_symbol = path.to_sym
         @paths[path_symbol] = File.join(@paths[:source], config[path_symbol] || path)
+
+        self.class.instance_eval do
+          define_method("#{path}_path") do
+            @paths[path_symbol]
+          end
+        end
       end
     end
 
-    def scan_files()
+    def scan_files
       Dir.glob(File.join(@paths[:templates], '*.*')).each do |path|
         template = Salt::Template.new(path)
         @templates[template.slug] = template
@@ -46,13 +50,48 @@ module Salt
       Dir.glob(File.join(@paths[:posts], '*.*')).each do |path|
         @posts << @klasses[:post].new(path)
       end
+
+      @posts.reverse!
     end
 
-    def generate()
-      self.scan_files()
+    def render_template(key, contents, context = {})
+      unless @templates.include?(key)
+        return contents
+      end
+
+      @templates[key].render(contents, context)
+    end
+
+    def generate
+      self.scan_files
+
+      begin
+        Dir.mkdir(@paths[:site]) unless Dir.exists?(@paths[:site])
+      rescue Exception => e
+        @error = "Failed to prepare the site directory (#{e})"
+        return
+      end
+
+      begin
+        @pages.each do |page|
+          page.write(@paths[:site])
+        end
+      rescue Exception => e
+        @error = "Failed to render a page (#{e})"
+        return
+      end
+
+      begin
+        @posts.each do |post|
+          post.write(File.join(@paths[:site], 'posts'))
+        end
+      rescue Exception => e
+        @error = "Failed to render a post (#{e})"
+        return
+      end
     end
 
     private
-      attr_accessor :klasses
+      attr_accessor :paths, :templates, :settings, :pages, :posts, :error, :klasses
   end
 end
