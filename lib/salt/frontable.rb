c1733d769ce35f60536f29d7056a2823c12fcfda
Daniel Bogan <d@waferbaby.com>
2013-09-30 23:08:18 -0700

    Wrapped the dynamic attr_accessor and variable setting in an add_metadata method.

--- lib/salt/frontable.rb
+++ lib/salt/frontable.rb
@@ -8,14 +8,18 @@ module Salt
         contents = parts.post_match.strip!
 
         metadata.each_pair do |key, value|
-          unless instance_variable_get("@#{key}")
-            self.class.send(:attr_accessor, key.to_sym)
-            instance_variable_set("@#{key}", value) 
-          end
+          self.add_metadata(key, value)
         end
       end
 
       contents
+    end
+
+    def add_metadata(key, value)
+      unless instance_variable_get("@#{key}")
+        self.class.send(:attr_accessor, key.to_sym)
+        instance_variable_set("@#{key}", value) 
+      end
     end
   end
 end
