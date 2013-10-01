ed758250ce547879c6ebc68197017dd33ad165f7
Daniel Bogan <d@waferbaby.com>
2013-09-30 21:33:48 -0700

    Removed the site from the initialize method.

--- lib/salt/post.rb
+++ lib/salt/post.rb
@@ -2,9 +2,9 @@ module Salt
   class Post < Page
     attr_accessor :slug, :date, :contents
 
-    def initialize(site, path)
-      super(site, path)
-      
+    def initialize(path)
+      super
+
       parts = File.basename(path, File.extname(path)).match(/(\d{4})-(\d{2})-(\d{2})-(.+)/)
 
       @slug = parts[4]
