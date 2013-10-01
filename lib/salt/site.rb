6b5cd97d97de9f67e76e589e98af3073e167ad30
Daniel Bogan <d@waferbaby.com>
2013-09-30 21:35:52 -0700

    Switched back to the instance method. Don't pass the site object to the templates, pages and posts. Set the public path, prepare for copying its contents.

--- lib/salt/site.rb
+++ lib/salt/site.rb
@@ -1,18 +1,25 @@
 module Salt
   class Site
-    def initialize(path = nil)
-      @paths, @templates, @pages, @posts = {}, {}, [], []
+    def self.instance
+      @site ||= new
+    end
 
-      @paths[:source] = path ? File.expand_path(path) : Dir.pwd
-      @paths[:site] = File.join(@paths[:source], 'site')
-      @paths[:pages] = File.join(@paths[:source], 'pages')
-      @paths[:posts] = File.join(@paths[:source], 'posts')
-      @paths[:templates] = File.join(@paths[:source], 'templates')
+    def initialize
+      @paths, @templates, @pages, @posts = {}, {}, [], []
 
       @klasses = {
         page: Salt::Page,
         post: Salt::Post,
       }
+    end
+
+    def setup(path = nil)
+      @paths[:source] = path ? File.expand_path(path) : Dir.pwd
+      @paths[:site] = File.join(@paths[:source], 'site')
+      @paths[:pages] = File.join(@paths[:source], 'pages')
+      @paths[:posts] = File.join(@paths[:source], 'posts')
+      @paths[:templates] = File.join(@paths[:source], 'templates')
+      @paths[:public] = File.join(@paths[:source], 'public')
     end
 
     def register(klass)
@@ -29,22 +36,22 @@ module Salt
 
     def scan_files
       Dir.glob(File.join(@paths[:templates], '*.*')).each do |path|
-        template = Salt::Template.new(self, path)
+        template = Salt::Template.new(path)
         @templates[template.slug] = template
       end
 
       Dir.glob(File.join(@paths[:pages], '**', '*.*')).each do |path|
-        @pages << @klasses[:page].new(self, path)
+        @pages << @klasses[:page].new(path)
       end
 
       Dir.glob(File.join(@paths[:posts], '*.*')).each do |path|
-        @posts << @klasses[:post].new(self, path)
+        @posts << @klasses[:post].new(path)
       end
 
       @posts.reverse!
     end
 
-    def render_template(key, contents, context = {})
+    def render_template(key, contents, context = Object.new)
       unless @templates.include?(key)
         return contents
       end
@@ -58,7 +65,7 @@ module Salt
       begin
         Dir.mkdir(@paths[:site]) unless Dir.exists?(@paths[:site])
       rescue Exception => e
-        @error = "Failed to prepare the site directory (#{e})"
+        puts "Failed to prepare the site directory (#{e})"
         return
       end
 
@@ -67,7 +74,7 @@ module Salt
           page.write(@paths[:site])
         end
       rescue Exception => e
-        @error = "Failed to render a page (#{e})"
+        puts "Failed to render a page (#{e})"
         return
       end
 
@@ -76,8 +83,14 @@ module Salt
           post.write(@paths[:site])
         end
       rescue Exception => e
-        @error = "Failed to render a post (#{e})"
+        puts "Failed to render a post (#{e})"
         return
+      end
+
+      begin
+        if Dir.exists?(@paths[:public])
+        end
+      rescue Exception => e
       end
     end
 
