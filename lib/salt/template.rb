af4f6f7c26a107d339c9e220a526e79bff46984f
Daniel Bogan <d@waferbaby.com>
2013-10-01 23:47:03 -0700

    Include renderable, and use its render method instead.

--- lib/salt/template.rb
+++ lib/salt/template.rb
@@ -1,23 +1,13 @@
 module Salt
   class Template
     include Frontable
+    include Renderable
 
     attr_accessor :slug, :contents, :parent
 
     def initialize(path)
       @slug = File.basename(path, File.extname(path))
       @contents = read_with_yaml(path)
-    end
-
-    def render(contents, context = {})
-      site = Site.instance
-
-      context[:site] ||= site
-
-      output = Erubis::Eruby.new(@contents).evaluate(context) { contents }
-      output = site.render_template(@layout, output, context) if @layout
-
-      output
     end
   end
 end
