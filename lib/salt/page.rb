c2484e8bf500664dd7a6db497485782d3fd99b39
Daniel Bogan <d@waferbaby.com>
2013-10-05 15:22:24 -0700

    Switched @contents to an attr_writer, and added the contents method. Switched output_path and write to take a site as their first param.

--- lib/salt/page.rb
+++ lib/salt/page.rb
@@ -3,7 +3,8 @@ module Salt
     include Frontable
     include Renderable
 
-    attr_accessor :path, :contents, :filename, :extension
+    attr_accessor :path, :filename, :extension
+    attr_writer :contents
 
     def initialize(path = nil)
       @path = path
@@ -18,18 +19,22 @@ module Salt
       end
     end
 
+    def contents
+      @contents
+    end
+
     def output_file(extension = nil)
       "#{self.filename}#{extension ? extension : self.extension}"
     end
 
-    def output_path(parent_path)
+    def output_path(site, parent_path)
       return parent_path if @path.nil?
-      File.join(parent_path, File.dirname(@path).gsub(Site.instance.path(:pages), ''))
+      File.join(parent_path, File.dirname(@path).gsub(site.path(:pages), ''))
     end
 
-    def write(path, context = {}, extension = nil)
-      contents = self.render(@contents, {this: self}.merge(context))
-      output_path = self.output_path(path)
+    def write(site, path, context = {}, extension = nil)
+      contents = self.render(site, @contents, {this: self}.merge(context))
+      output_path = self.output_path(site, path)
 
       FileUtils.mkdir_p(output_path) unless Dir.exists?(output_path)
       full_path = File.join(output_path, self.output_file(extension))
