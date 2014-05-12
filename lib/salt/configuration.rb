d67a2fb26605274166a8d3449b417f1b6226d8a8
Daniel Bogan <d@waferbaby.com>
2014-05-11 23:55:01 -0700

    Initial import.

---
+++ lib/salt/configuration.rb
@@ -0,0 +1,56 @@
+module Salt
+  class Configuration
+    def initialize(config = {})
+      @settings = Salt::Configuration.default_settings
+
+      @settings.each_key do |key|
+        @settings[key] = config[key] if config.key?(key)
+      end
+    end
+
+    def [](key)
+      @settings[key]
+    end
+
+    def self.default_settings
+      {
+        root: Dir.pwd,
+
+        markdown: {
+          enabled: true,
+          options: {}
+        },
+
+        pagination: {
+          enabled: true,
+          per_page: 10,
+        },
+
+        generation: {
+          categories: true,
+          year_archives: true,
+          month_archives: true,
+          day_archives: true,
+          feed: true,
+          category_feeds: true,
+        },
+
+        date_formats: {
+          year: '%Y',
+          month: '%Y-%m',
+          day: '%Y-%m-%d',
+        },
+
+        output: {
+          site: 'site',
+          posts: 'archives'
+        },
+
+        layouts: {
+          listing: 'posts',
+          category: 'category'
+        }
+      }
+    end
+  end
+end
