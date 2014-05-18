51a85b4e070668a6e0cab01a512f2217eb05fe91
Daniel Bogan <d@waferbaby.com>
2014-05-17 22:08:49 -0700

    Handle being passed an actual .yml file, in which case just read the whole thing in.

--- lib/salt/frontable.rb
+++ lib/salt/frontable.rb
@@ -1,13 +1,20 @@
 module Salt
   module Frontable
     def read_with_yaml(path)
-      contents = File.read(path)
-      matches = contents.match(/^(-{3}\n.*?\n?)^(-{3}*$\n?)/m)
+      if path =~ /.yml$/
+        contents = ''
+        metadata = YAML.load_file(path)
+      else
+        contents = File.read(path)
+        matches = contents.match(/^(-{3}\n.*?\n?)^(-{3}*$\n?)/m)
 
-      if matches
-        metadata = YAML.load(matches[1])
-        contents = matches.post_match.strip!
+        if matches
+          metadata = YAML.load(matches[1])
+          contents = matches.post_match.strip!
+        end
+      end
 
+      if metadata
         metadata.each_pair do |key, value|
           set_metadata(key, value)
         end
