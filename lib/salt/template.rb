4dad392f263c418430f270cbc230f6b867d83459
Daniel Bogan <d@waferbaby.com>
2015-02-08 23:12:26 -0800

    Switched to Publishable, broke out the attr_accessors.

--- lib/salt/template.rb
+++ lib/salt/template.rb
@@ -1,15 +1,19 @@
 module Salt
   class Template
     include Frontable
-    include Renderable
+    include Publishable
 
-    attr_accessor :slug, :title, :path, :contents, :parent
+    attr_accessor :slug
+    attr_accessor :title
+    attr_accessor :path
+    attr_accessor :contents
+    attr_accessor :parent
 
     def initialize(site, path)
     	@site = site
-
       @slug = path.match(/#{@site.source_paths[:templates]}#{File::SEPARATOR}?(.+)\.erb/)[1]
       @path = path
+
       @contents = read_with_yaml(path)
     end
   end
