f5fafa9a174a272629d8a91c135f5450f47ff12e
Daniel Bogan <d@waferbaby.com>
2015-02-08 23:41:32 -0800

    Switched to Publishable, broke out the attr_accessors, switched to using the extension defined in the config, and removed the output_file and write methods.

--- lib/salt/page.rb
+++ lib/salt/page.rb
@@ -1,56 +1,37 @@
 module Salt
   class Page
     include Frontable
-    include Renderable
+    include Publishable
 
-    attr_accessor :path, :title, :contents, :filename, :extension, :layout
+    attr_accessor :path
+    attr_accessor :title
+    attr_accessor :contents
+    attr_accessor :filename
+    attr_accessor :extension
+    attr_accessor :layout
 
     def initialize(site, path = nil)
       @site = site
-      @path = path
-      @title = false
-      @extension = 'html'
-
+      
       if path
+        @path = path
         @contents = read_with_yaml(path)
         @filename = File.basename(path, File.extname(path))
       else
-        @contents = ''
         @filename = 'index'
       end
+
+      @extension = @site.config['file_extensions']['pages']
+      @layout ||= @site.config['layouts']['page']
     end
 
     def type
       :page
     end
 
-    def output_file
-      "#{filename}.#{extension}"
-    end
-
     def output_path(parent_path)
       return parent_path if @path.nil?
-
       File.join(parent_path, File.dirname(@path).gsub(@site.source_paths[:pages], ''))
-    end
-
-    def write(path, context = false)
-      directory_path = output_path(path)
-      full_path = File.join(directory_path, output_file)
-
-      @page_url = full_path.gsub(@site.output_paths[:site], '').gsub(/index\.html$/, '')
-      
-      contents = if context
-        render(@site, @contents, {this: self}.merge(context))
-      else
-        @contents
-      end
-
-      FileUtils.mkdir_p(directory_path) unless Dir.exist?(directory_path)
-
-      File.open(full_path, 'w') do |file|
-        file.write(contents)
-      end
     end
   end
 end
