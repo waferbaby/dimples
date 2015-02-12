caf81e7cb98011b6d711220e215f67e570804c4a
Daniel Bogan <d@waferbaby.com>
2015-02-11 16:31:11 -0800

    Switched over to output_file_path, which returns the file name too now.

--- lib/salt/publishable.rb
+++ lib/salt/publishable.rb
@@ -1,12 +1,14 @@
 module Salt
   module Publishable
     def write(path, context = false)
-      directory_path = output_path(path)
       contents = context ? render(@contents, {this: self}.merge(context)) : @contents
 
-      FileUtils.mkdir_p(directory_path) unless Dir.exist?(directory_path)
+      publish_path = output_file_path(path)
+      parent_path = File.dirname(publish_path)
 
-      File.open(File.join(directory_path, "#{@filename}.#{@extension}"), 'w') do |file|
+      FileUtils.mkdir_p(parent_path) unless Dir.exist?(parent_path)
+
+      File.open(publish_path, 'w') do |file|
         file.write(contents)
       end
     end
