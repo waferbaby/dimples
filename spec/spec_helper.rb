890f71dce19a5695bd1815b29b025d09f57c13bd
Daniel Bogan <d@waferbaby.com>
2015-02-19 12:22:03 -0800

    Added the before and after all options for creating the site object and removing the test site output directory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,24 @@
+require 'fileutils'
+
 RSpec.configure do |config|
+  config.before(:all) do
+    current_path = File.dirname(__dir__)
+
+    @site = Dimples::Site.new({
+      'source_path' => File.join(current_path, 'spec', 'fixtures'),
+      'destination_path' => File.join(current_path, 'test_site')
+    })
+
+    @site.scan_files
+  end
+
+  config.after(:all) do
+    begin
+      FileUtils.remove_dir(@site.output_paths[:site])
+    rescue
+    end
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
