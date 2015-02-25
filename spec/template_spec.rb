57fefff15d012d8d28199c58ebb5dc7e19bf95b9
Daniel Bogan <d@waferbaby.com>
2015-02-24 22:34:07 -0800

    Initial import.

---
+++ spec/template_spec.rb
@@ -0,0 +1,23 @@
+require 'dimples'
+
+describe 'A template', publishing: true do
+
+  subject { @site.templates['default'] }
+    
+  it 'should render' do
+    expected = <<EXPECTED
+<!DOCTYPE html>
+<html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title></title>
+</head>
+<body>
+Hello
+</body>
+</html>
+EXPECTED
+
+    expect(subject.render({}, 'Hello')).to eql(expected.rstrip)
+  end
+
+end
