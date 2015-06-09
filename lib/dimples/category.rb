bbbd812014458e43320fa24bdf425b2df9b2b41f
Daniel Bogan <d@waferbaby.com>
2015-06-09 00:35:15 -0700

    Updated to require a path.

--- lib/dimples/category.rb
+++ lib/dimples/category.rb
@@ -3,14 +3,16 @@ module Dimples
     include Frontable
 
     attr_accessor :slug
+    attr_accessor :path
     attr_accessor :name
     attr_accessor :posts
 
-    def initialize(slug, path = nil)
+    def initialize(slug, path)
       @slug = slug
+      @path = path
       @name = @slug.capitalize
 
-      read_with_yaml(path) if path
+      read_with_yaml(path)
 
       @posts = []
     end
