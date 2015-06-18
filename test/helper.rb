9b7c240e5debffda6ddb10a956dc242ce0947142
Daniel Bogan <d@waferbaby.com>
2015-06-17 23:39:25 -0700

    Added the test_site method.

--- test/helper.rb
+++ test/helper.rb
@@ -5,7 +5,9 @@ require "codeclimate-test-reporter"
 
 CodeClimate::TestReporter.start
 
-@site = Dimples::Site.new({
-  'source_path' => File.join(__dir__),
-  'destination_path' => File.join(__dir__, 'test_site')
-})
+def test_site
+  Dimples::Site.new({
+    'source_path' => File.join(__dir__),
+    'destination_path' => File.join(__dir__, 'test_site')
+  })
+end
