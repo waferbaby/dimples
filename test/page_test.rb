3e4464e11b7db1e3fd7dd7c5fc1ea2d460ca8803
Daniel Bogan <d@waferbaby.com>
2015-08-16 18:24:37 -0700

    Switched to about.markdown, and adjusted the expected output.

--- test/page_test.rb
+++ test/page_test.rb
@@ -1,11 +1,10 @@
 $:.unshift(__dir__)
 
 require 'helper'
-require 'minitest/autorun'
 
 describe "Page" do
   before { @site = test_site }
-  subject { Dimples::Page.new(@site, File.join(@site.source_paths[:pages], 'about.erb')) }
+  subject { Dimples::Page.new(@site, File.join(@site.source_paths[:pages], 'about.markdown')) }
 
   it "parses its YAML frontmatter" do
     assert_equal 'About', subject.title
@@ -14,7 +13,8 @@ describe "Page" do
 
   it "renders its contents" do
     expected_output = "<h2>About this site</h2>
-<p>Hello! I'm an about page.</p>"
+
+<p>Hello! I&#39;m an about page.</p>"
 
     assert_equal expected_output, subject.render
   end
