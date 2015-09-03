fc842fb77a87e5b9ad77375fda556a953f15ddcd
Daniel Bogan <d@waferbaby.com>
2015-09-02 22:06:57 -0700

    Added the tilt/redcarpet require, and updated the test_site method to only create the site once.

--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,12 @@
 require 'minitest/autorun'
 require 'codeclimate-test-reporter'
+require 'tilt/redcarpet'
 require 'dimples'
 
 CodeClimate::TestReporter.start
 
 def test_site
-  Dimples::Site.new({
+  @site ||= Dimples::Site.new({
     'source_path' => File.join(__dir__, 'site'),
     'destination_path' => File.join(__dir__, 'build')
   })
