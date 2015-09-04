9e5e2a6c436a4fe8ce432b7875123e7bca9e00c6
Daniel Bogan <d@waferbaby.com>
2015-09-03 17:25:02 -0700

    Fleshed out the template test.

--- test/test_template.rb
+++ test/test_template.rb
@@ -1,1 +1,24 @@
 $:.unshift(__dir__)
+
+require 'helper'
+
+describe "Template" do
+  before { @site = test_site }
+  subject { Dimples::Template.new(@site, File.join(@site.source_paths[:templates], 'default.erb')) }
+
+  it "renders its contents" do
+    expected_output = <<EXPECTED
+<!DOCTYPE html>
+<html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>My site</title>
+</head>
+<body>
+Welcome
+</body>
+</html>
+EXPECTED
+
+    assert_equal expected_output.strip, subject.render({}, 'Welcome')
+  end
+end
