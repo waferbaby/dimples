fb3f047d1b4c953ea688c6173f979c304c5f73be
Daniel Bogan <d@waferbaby.com>
2015-09-13 14:05:19 -0700

    Updated the template count test.

--- test/test_site.rb
+++ test/test_site.rb
@@ -11,12 +11,10 @@ describe "Site" do
   end
 
   describe "scanning files" do
-    before do
-      subject.scan_files
-    end
+    before { subject.scan_files }
 
     it "finds all the templates" do
-      assert_equal 3, subject.templates.length
+      assert_equal 4, subject.templates.length
     end
 
     it "finds all the pages" do
@@ -25,6 +23,35 @@ describe "Site" do
 
     it "finds all the posts" do
       assert_equal 2, subject.posts.length
+    end
+  end
+
+  it "generates categories" do
+    subject.scan_files
+    subject.generate_categories
+
+    subject.categories.keys.each do |slug|
+      expected_output = <<OUTPUT
+<!DOCTYPE html>
+<html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>My site</title>
+</head>
+<body>
+<h2>#{slug.upcase} Posts</h2>
+
+<ul>
+  <li><a href="/archives/2015/01/01/another-post/">My second post</a></li>
+  <li><a href="/archives/2015/01/01/a-post/">My first post</a></li>
+</ul>
+</body>
+</html>
+OUTPUT
+
+      category_file_path = File.join(subject.output_paths[:posts], slug, "index.html")
+
+      assert_equal true, File.exist?(category_file_path)
+      assert_equal expected_output.strip, File.read(category_file_path)
     end
   end
 end
