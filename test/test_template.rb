913e303d0313769a1266c622f94dce78425add4c
Daniel Bogan <d@waferbaby.com>
2016-02-27 10:57:55 -0800

    Switched from the inline output to the template fixture.

--- test/test_template.rb
+++ test/test_template.rb
@@ -7,18 +7,7 @@ describe "Template" do
   subject { Dimples::Template.new(@site, File.join(@site.source_paths[:templates], 'default.erb')) }
 
   it "renders its contents" do
-    expected_output = <<EXPECTED
-<!DOCTYPE html>
-<html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>My site</title>
-</head>
-<body>
-Welcome
-</body>
-</html>
-EXPECTED
-
-    subject.render({}, 'Welcome').must_equal(expected_output.strip)
+    expected_output = render_fixture('template.erb')
+    subject.render({}, 'Welcome').must_equal(expected_output)
   end
 end
