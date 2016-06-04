1b3ecd92f27d7752bfa8b01db32ab6fcb16a8b1d
Daniel Bogan <d@waferbaby.com>
2016-06-03 22:34:44 -0700

    Split out the default sections into their own methods.

--- lib/dimples/configuration.rb
+++ lib/dimples/configuration.rb
@@ -26,52 +26,67 @@ module Dimples
       {
         'source_path' => current_path,
         'destination_path' => File.join(current_path, 'site'),
+        'rendering' => {},
+        'category_names' => {},
+        'paths' => default_paths,
+        'layouts' => default_layouts,
+        'pagination' => default_pagination,
+        'generation' => default_generation,
+        'file_extensions' => default_file_extensions,
+        'date_formats' => default_date_formats
+      }
+    end
 
-        'paths' => {
-          'archives' => 'archives',
-          'posts' => 'archives/%Y/%m/%d',
-          'categories' => 'archives/categories'
-        },
+    def self.default_layouts
+      {
+        'posts' => 'posts',
+        'post' => 'post',
+        'category' => 'category',
+        'year_archives' => 'year_archives',
+        'month_archives' => 'month_archives',
+        'day_archives' => 'day_archives'
+      }
+    end
 
-        'layouts' => {
-          'posts' => 'posts',
-          'post' => 'post',
-          'category' => 'category',
-          'year_archives' => 'year_archives',
-          'month_archives' => 'month_archives',
-          'day_archives' => 'day_archives'
-        },
+    def self.default_paths
+      {
+        'archives' => 'archives',
+        'posts' => 'archives/%Y/%m/%d',
+        'categories' => 'archives/categories'
+      }
+    end
 
-        'rendering' => {
-        },
+    def self.default_pagination
+      {
+        'enabled' => true,
+        'per_page' => 10
+      }
+    end
 
-        'pagination' => {
-          'enabled' => true,
-          'per_page' => 10
-        },
+    def self.default_generation
+      {
+        'paginated_posts' => false,
+        'categories' => true,
+        'year_archives' => true,
+        'month_archives' => true,
+        'day_archives' => true,
+        'feed' => true,
+        'category_feeds' => true
+      }
+    end
 
-        'generation' => {
-          'paginated_posts' => false,
-          'categories' => true,
-          'year_archives' => true,
-          'month_archives' => true,
-          'day_archives' => true,
-          'feed' => true,
-          'category_feeds' => true
-        },
+    def self.default_file_extensions
+      {
+        'pages' => 'html',
+        'posts' => 'html'
+      }
+    end
 
-        'file_extensions' => {
-          'pages' => 'html',
-          'posts' => 'html'
-        },
-
-        'date_formats' => {
-          'year' => '%Y',
-          'month' => '%Y-%m',
-          'day' => '%Y-%m-%d'
-        },
-
-        'category_names' => {}
+    def self.default_date_formats
+      {
+        'year' => '%Y',
+        'month' => '%Y-%m',
+        'day' => '%Y-%m-%d'
       }
     end
   end
