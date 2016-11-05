fc86cb65099109b299da485512d67287e3363e21
Daniel Bogan <d@waferbaby.com>
2016-11-05 11:21:45 +1100

    Removed the call to output_file_path.

--- lib/dimples/writeable.rb
+++ lib/dimples/writeable.rb
@@ -2,18 +2,16 @@ module Dimples
   module Writeable
     def write(path, context = {})
       output = context ? render(context) : contents
-
-      publish_path = output_file_path(path)
-      parent_path = File.dirname(publish_path)
+      parent_path = File.dirname(path)
 
       begin
         FileUtils.mkdir_p(parent_path) unless Dir.exist?(parent_path)
 
-        File.open(publish_path, 'w+') do |file|
+        File.open(path, 'w+') do |file|
           file.write(output)
         end
       rescue SystemCallError => e
-        raise Errors::PublishingError.new(publish_path, e.message)
+        raise Errors::PublishingError.new(path, e.message)
       end
     end
   end
