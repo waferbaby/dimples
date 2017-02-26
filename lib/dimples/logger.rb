42ff96591c57b40be44041246bd6c514c81ffd6e
Daniel Bogan <d@waferbaby.com>
2017-02-26 22:48:49 +1100

    Added our Logger subclass, which includes the debug_generation method, and sets the LogFormatter class on itself. The latter now prefixes output with a dash if it's a debug message.

--- lib/dimples/logger.rb
+++ lib/dimples/logger.rb
@@ -1,7 +1,22 @@
 module Dimples
+  class Logger < Logger
+    def initialize(*)
+      super
+      @formatter = LogFormatter
+    end
+
+    def debug_generation(type, count)
+      message = "Generating #{type} (#{count} item"
+      message += 's' if count != 1
+      message += ')...'
+
+      debug(message)
+    end
+  end
+
   class LogFormatter < Logger::Formatter
     def self.call(severity, time, program_name, message)
-      "#{time.strftime('%r')}: #{message}\n"
+      "#{time.strftime('%r')}: #{'- ' if severity == 'DEBUG'}#{message}\n"
     end
   end
 end
