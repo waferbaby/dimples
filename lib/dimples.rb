f8b9199ac346b4e9cdf6cdbefe73fda012689f1d
Daniel Bogan <d@waferbaby.com>
2017-02-26 17:14:00 +1100

    Added logger, dimples/logger, and set up the module's logger.

--- lib/dimples.rb
+++ lib/dimples.rb
@@ -3,8 +3,10 @@ $LOAD_PATH.unshift(__dir__)
 require 'fileutils'
 require 'yaml'
 require 'tilt'
+require 'logger'
 
 require 'dimples/errors'
+require 'dimples/logger'
 
 require 'dimples/frontable'
 require 'dimples/writeable'
@@ -15,3 +17,13 @@ require 'dimples/page'
 require 'dimples/post'
 require 'dimples/site'
 require 'dimples/template'
+
+module Dimples
+  class << self
+    def logger
+      @logger ||= Logger.new(STDOUT).tap do |logger|
+        logger.formatter = Dimples::LogFormatter
+      end
+    end
+  end
+end
