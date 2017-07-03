14a672b09386ca3607da1afe318a734c5ede7ce7
Daniel Bogan <d@waferbaby.com>
2017-07-03 22:33:23 +1000

    Make Post a subclass of Page.

--- lib/dimples/post.rb
+++ lib/dimples/post.rb
@@ -2,24 +2,12 @@
 
 module Dimples
   # A class that models a single site post.
-  class Post
-    include Frontable
-    include Writeable
-    include Renderable
-
-    attr_accessor :path
-    attr_accessor :title
+  class Post < Page
     attr_accessor :categories
-    attr_accessor :template
-    attr_accessor :filename
-    attr_accessor :extension
-    attr_accessor :layout
-    attr_accessor :contents
     attr_accessor :slug
     attr_accessor :year
     attr_accessor :month
     attr_accessor :day
-    attr_accessor :rendered_contents
     attr_accessor :previous_post
     attr_accessor :next_post
     attr_reader :date
@@ -27,18 +15,15 @@ module Dimples
     FILENAME_DATE = /(\d{4})-(\d{2})-(\d{2})-(.+)/
 
     def initialize(site, path)
-      @site = site
-      @path = path
-      @filename = 'index'
-      @extension = 'html'
+      super(site, path)
 
       parts = File.basename(path, File.extname(path)).match(FILENAME_DATE)
 
+      @filename = 'index'
       @slug = parts[4]
-      self.date = Time.mktime(parts[1], parts[2], parts[3])
-
       @layout = @site.config['layouts']['post']
-      @contents = read_with_front_matter(path)
+
+      self.date = Time.mktime(parts[1], parts[2], parts[3])
     end
 
     def date=(date)
