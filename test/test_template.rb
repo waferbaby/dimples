5bf9eedc8c7747fc9df6bae69cd5d0c5abf0fd97
Daniel Bogan <d@waferbaby.com>
2017-08-20 04:39:42 +1000

    Switch to calling read_fixture, and have the test site scan its files.

--- test/test_template.rb
+++ test/test_template.rb
@@ -5,13 +5,15 @@ $LOAD_PATH.unshift(__dir__)
 require 'helper'
 
 describe 'Template' do
+  before { test_site.scan_files }
+
   subject do
     path = File.join(test_site.source_paths[:templates], 'default.erb')
     Dimples::Template.new(test_site, path)
   end
 
   it 'renders its contents' do
-    expected_output = render_template('template')
+    expected_output = read_fixture('templates/default')
     subject.render({}, 'Welcome').must_equal(expected_output)
   end
 end
