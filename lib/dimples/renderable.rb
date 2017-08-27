b3427009215290e6bbe4060a2c2dc81e2b312440
Daniel Bogan <d@waferbaby.com>
2017-08-28 00:47:38 +1000

    Add the Renderable mixin.

---
+++ lib/dimples/renderable.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Dimples
+  # A mixin class that allows a document to render.
+  module Renderable
+    attr_accessor :rendered_contents
+
+    def render(context = {}, body = nil)
+      context[:site] ||= @site
+      context[:this] ||= self
+      context[:type] ||= self.class.name.split('::').last.downcase.to_sym
+
+      scope = Object.new.tap do |scope|
+        context.each_pair do |key, value|
+          scope.instance_variable_set("@#{key}".to_sym, value)
+        end
+      end
+
+      output = rendering_engine.render(scope) { body }.strip
+      rendered_contents = output
+
+      if @site.templates[layout]
+        @site.templates[layout].render(context, output)
+      else
+        output
+      end
+    end
+
+    def rendering_engine
+      @rendering_engine ||= begin
+        callback = proc { contents }
+
+        if @path
+          ext = File.extname(@path)[1..-1]
+          options = @site.config['rendering'][ext] || {}
+          Tilt.new(@path, options, &callback)
+        else
+          Tilt::StringTemplate.new(&callback)
+        end
+      end
+    end
+  end
+end
