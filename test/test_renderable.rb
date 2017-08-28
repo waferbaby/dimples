7a2c24d4d31312f562b21d728d262031cfe90ee2
Daniel Bogan <d@waferbaby.com>
2017-08-29 07:38:59 +1000

    Rename to test_renderable.rb and update the tests for the new module.

--- test/test_renderable.rb
+++ test/test_renderable.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(__dir__)
 
 require 'helper'
 
-describe 'Renderer' do
+describe 'Renderable' do
   before do
     test_site.scan_templates
   end
@@ -15,10 +15,11 @@ describe 'Renderer' do
         base_path = test_site.source_paths[:pages]
         file_path = File.join(base_path, 'about', 'contact.markdown')
 
-        Dimples::Renderer.new(
-          test_site,
-          Dimples::Page.new(test_site, file_path)
-        )
+        Dimples::Page.new(test_site, file_path)
+      end
+
+      it 'maps to the correct Tilt engine for the given source file' do
+        subject.rendering_engine.class.must_equal(Tilt[subject.path])
       end
 
       it 'renders the expected output' do
@@ -41,13 +42,15 @@ describe 'Renderer' do
     end
 
     describe 'using a source without a file path' do
-      let(:page) do
+      subject do
         Dimples::Page.new(test_site).tap do |page|
           page.contents = 'Welcome!'
         end
       end
 
-      subject { Dimples::Renderer.new(test_site, page) }
+      it 'maps to the Tilt string template engine' do
+        subject.rendering_engine.class.must_equal(Tilt::StringTemplate)
+      end
 
       describe 'and no layout' do
         it 'renders the contents as is' do
@@ -56,7 +59,7 @@ describe 'Renderer' do
       end
 
       describe 'with a layout' do
-        before { page.layout = 'default' }
+        before { subject.layout = 'default' }
 
         it 'renders the contents with the template' do
           expected_output = read_fixture('pages/general/welcome')
