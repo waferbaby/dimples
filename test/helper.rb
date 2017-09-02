f8361e678ce0dd40aa165e9d633f9291f3ef3231
Daniel Bogan <d@waferbaby.com>
2017-09-02 15:27:52 +1000

    Remove most of the helper methods, switch to using LittleFixtures.

--- test/helper.rb
+++ test/helper.rb
@@ -15,44 +15,17 @@ end
 def test_configuration
   @config ||= Dimples::Configuration.new(
     'source_path' => File.join(__dir__, 'source'),
-    'destination_path' => site_destination
+    'destination_path' => File.join(
+      File::SEPARATOR, 'tmp', "dimples-#{Time.new.to_i}"
+    )
   )
 end
 
-def site_destination
-  File.join(File::SEPARATOR, 'tmp', "dimples-#{Time.new.to_i}")
-end
-
-def site_feed_template_types
-  test_site.feed_templates.map do |template|
-    template.split('.')[1]
-  end
-end
-
-def read_fixture(filename)
-  File.read(File.join(__dir__, 'fixtures', filename))
-end
-
-def compare_file_to_fixture(file_path, fixture_name)
-  expected_output = read_fixture(fixture_name)
-  File.read(file_path).must_equal(expected_output)
+def fixtures
+  @fixtures ||= LittleFixtures.load(File.join(__dir__, 'fixtures'))
 end
 
 def clean_generated_site
   files = File.join(test_site.output_paths[:site], '*')
   FileUtils.rm_r(Dir.glob(files), force: true)
-end
-
-def archive_file_paths(date_type)
-  test_site.archives[date_type].each_key.map do |date|
-    dates = date.split('/')
-
-    file_path = File.join(
-      test_site.output_paths[:archives],
-      dates,
-      'index.html'
-    )
-
-    [dates.join('-'), file_path]
-  end
 end
