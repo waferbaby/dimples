812f5c44e7b5ad7ff38243925cf8d827059caaa7
Daniel Bogan <d@waferbaby.com>
2017-10-30 23:24:41 +1100

    Add tests for debug_generation

--- test/test_logger.rb
+++ test/test_logger.rb
@@ -8,6 +8,24 @@ describe Dimples::Logger do
   before { @output = StringIO.new }
   subject { Dimples::Logger.new(@output) }
 
+  describe 'when calling debug_generation' do
+    describe 'with one item' do
+      before { subject.debug_generation('page', 1) }
+
+      it 'shows the correctly formatted message' do
+        @output.string.must_equal("\e[93m- Generating page (1 item)...\e[0m\n")
+      end
+    end
+
+    describe 'with more than one item' do
+      before { subject.debug_generation('page', 2) }
+
+      it 'shows the correctly formatted message' do
+        @output.string.must_equal("\e[93m- Generating page (2 items)...\e[0m\n")
+      end
+    end
+  end
+
   describe 'when showing an error' do
     before { subject.error('This is an error') }
 
