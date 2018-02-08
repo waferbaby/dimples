ca72cdc5ceb814c526e1f9406ea08996f1abe2c8
Daniel Bogan <d@waferbaby.com>
2018-02-09 08:25:57 +1100

    Flesh out the plugin class

--- lib/dimples/plugin.rb
+++ lib/dimples/plugin.rb
@@ -1,11 +1,40 @@
 module Dimples
-  class Filter
-    def self.process(action, item, &block)
-      puts "I am before #{action} with #{item}"
+  class Plugin
+    EVENTS = %i[
+      post_write
+      page_write
+    ].freeze
 
-      puts yield
+    def self.inherited(subclass)
+      (@subclasses ||= []) << subclass
+    end
 
-      puts "I am after #{action} with #{item}"
+    def self.plugins(site)
+      @plugins ||= {}.tap do |plugins|
+        @subclasses.each do |subclass|
+          plugin = subclass.new(site)
+
+          plugin.supported_events.each do |event|
+            (plugins[event] ||= []) << plugin
+          end
+        end
+      end
+    end
+
+    def self.process(site, action, item, &block)
+      plugins(site)[action]&.each { |plugin| plugin.process(action, item, &block) }
+      yield
+    end
+
+    def initialize(site)
+      @site = site
+    end
+
+    def process(action, item, &block)
+    end
+
+    def supported_events
+      []
     end
   end
 end
