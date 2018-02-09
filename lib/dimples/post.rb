b3067e13531f9c10c77070cc75d721abe6c1f814
Daniel Bogan <d@waferbaby.com>
2018-02-09 16:47:08 +1100

    Rename the name regex constant, switch to the right layout, add the output_directory method

--- lib/dimples/post.rb
+++ lib/dimples/post.rb
@@ -1,18 +1,32 @@
+# frozen_string_literal: true
+
 module Dimples
   class Post < Page
-    FILENAME_DATE = /^(\d{4})-(\d{2})-(\d{2})/
+    POST_FILENAME = /(\d{4})-(\d{2})-(\d{2})-(.+)/
 
     def initialize(site, path)
       super
 
-      parts = File.basename(path, File.extname(path)).match(FILENAME_DATE)
+      parts = File.basename(path, File.extname(path)).match(POST_FILENAME)
 
-      @metadata[:layout] = 'post'
+      @metadata[:layout] ||= @site.config.layouts.post
+
       @metadata[:date] = Date.new(parts[1].to_i, parts[2].to_i, parts[3].to_i)
-
       @metadata[:year] = @metadata[:date].strftime('%Y')
       @metadata[:month] = @metadata[:date].strftime('%m')
       @metadata[:day] = @metadata[:date].strftime('%d')
+
+      @metadata[:slug] = parts[4]
+    end
+
+    private
+
+    def output_directory
+      @output_directory ||= File.join(
+        @site.paths[:output],
+        date.strftime(@site.config.urls.posts),
+        slug
+      )
     end
   end
 end
