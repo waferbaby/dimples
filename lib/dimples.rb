761eb11e1927ebb73ce1cf3fc6428f8afd5a4dba
Daniel Bogan <d@waferbaby.com>
2018-02-09 16:44:14 +1100

    Remove the logger and unused gems, swap in the new ones

--- lib/dimples.rb
+++ lib/dimples.rb
@@ -2,30 +2,15 @@
 
 $LOAD_PATH.unshift(__dir__)
 
-require 'benchmark'
-require 'fileutils'
-require 'logger'
+require 'hashie'
 require 'tilt'
 require 'yaml'
 
-require 'dimples/frontable'
-require 'dimples/renderable'
-
-require 'dimples/category'
 require 'dimples/configuration'
 require 'dimples/errors'
-require 'dimples/logger'
+require 'dimples/frontable'
 require 'dimples/page'
-require 'dimples/pagination'
+require 'dimples/plugin'
 require 'dimples/post'
 require 'dimples/site'
 require 'dimples/template'
-
-# A static site generator.
-module Dimples
-  class << self
-    def logger
-      @logger ||= Dimples::Logger.new(STDOUT)
-    end
-  end
-end
