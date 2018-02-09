0afd8ccbdbad841caf4fa1fecf8ce2290c06fff2
Daniel Bogan <d@waferbaby.com>
2018-02-10 09:49:49 +1100

    Require renderable

--- lib/dimples.rb
+++ lib/dimples.rb
@@ -6,9 +6,11 @@ require 'hashie'
 require 'tilt'
 require 'yaml'
 
+require 'dimples/frontable'
+require 'dimples/renderable'
+
 require 'dimples/configuration'
 require 'dimples/errors'
-require 'dimples/frontable'
 require 'dimples/page'
 require 'dimples/plugin'
 require 'dimples/post'
