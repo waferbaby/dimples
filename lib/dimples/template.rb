80c92376e67513384c81b135a5520c3350385c7c
Daniel Bogan <d@waferbaby.com>
2018-02-10 09:48:13 +1100

    Include Renderable, add attr_accessors and set the path

--- lib/dimples/template.rb
+++ lib/dimples/template.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 module Dimples
   class Template
     include Frontable
+    include Renderable
+
+    attr_accessor :path
+    attr_accessor :contents
+    attr_accessor :metadata
 
     def initialize(site, path)
       @site = site
+      @path = path
       @contents, @metadata = read_with_front_matter(path)
-    end
-
-    def render(page, context)
-      payload = page.metadata.merge(context)
-
-      puts "The payload for #{page} will be: #{payload}"
-      # bubble up only if it itself has a template?
     end
   end
 end
