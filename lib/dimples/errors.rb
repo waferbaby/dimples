0946ab36ccd0b1830b2d77a3a4da2c4a4ec9f0d1
Daniel Bogan <d@waferbaby.com>
2018-02-09 16:43:05 +1100

    Add the error classes

---
+++ lib/dimples/errors.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Dimples
+  class PublishingError < StandardError
+  end
+
+  class RenderingError < StandardError
+  end
+
+  class GenerationError < StandardError
+  end
+end
