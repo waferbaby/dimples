90cbc5af270fa744257f0f618efa6745a5591f17
Daniel Bogan <d@waferbaby.com>
2018-02-11 02:23:34 +1100

    Remove the date parts and set a default empty categories array

--- lib/dimples/post.rb
+++ lib/dimples/post.rb
@@ -12,11 +12,8 @@ module Dimples
       @metadata[:layout] ||= @site.config.layouts.post
 
       @metadata[:date] = Date.new(parts[1].to_i, parts[2].to_i, parts[3].to_i)
-      @metadata[:year] = @metadata[:date].strftime('%Y')
-      @metadata[:month] = @metadata[:date].strftime('%m')
-      @metadata[:day] = @metadata[:date].strftime('%d')
-
       @metadata[:slug] = parts[4]
+      @metadata[:categories] ||= []
     end
 
     private
