522f81f1e07a470f5e0507a5e982b332cdd9c27f
Daniel Bogan <d@waferbaby.com>
2018-02-11 02:23:46 +1100

    Add the category class

---
+++ lib/dimples/category.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Dimples
+  class Category
+    attr_accessor :name
+    attr_accessor :slug
+    attr_accessor :posts
+
+    def initialize(site, slug)
+      @site = site
+      @slug = slug
+      @name = @site.config.category_names[slug.to_sym] || slug.capitalize
+
+      @posts = []
+    end
+
+    def inspect
+      "#<#{self.class} @slug=#{@slug} @name=#{@name}>"
+    end
+  end
+end
