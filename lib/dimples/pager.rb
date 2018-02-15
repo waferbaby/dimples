dbab5ac64c53b47ddf31c1044e1af7e4bfc8a1bf
Daniel Bogan <d@waferbaby.com>
2018-02-15 17:51:56 +1100

    Add the Pager class

---
+++ lib/dimples/pager.rb
@@ -0,0 +1,77 @@
+module Dimples
+  class Pager
+    include Enumerable
+
+    attr_reader :current_page
+    attr_reader :previous_page
+    attr_reader :next_page
+    attr_reader :page_count
+    attr_reader :item_count
+
+    def initialize(url, items, per_page)
+      @url = url
+      @items = items
+      @per_page = per_page
+      @page_count = (items.length.to_f / per_page.to_i).ceil
+
+      step_to(1)
+    end
+
+    def each(&block)
+      (1..@page_count).each do |index|
+        block.yield step_to(index), items_at(index)
+      end
+    end
+
+    def step_to(page)
+      @current_page = (1..@page_count).cover?(page) ? page : 1
+      @previous_page = (@current_page - 1).positive? ? @current_page - 1 : nil
+      @next_page = @current_page + 1 <= @page_count ? @current_page + 1 : nil
+
+      @current_page
+    end
+
+    def items_at(page)
+      @items.slice((page - 1) * @per_page, @per_page)
+    end
+
+    def current_page_url
+      @current_page != 1 ? "#{@url}#{@page_prefix}#{@current_page}" : @url
+    end
+
+    def first_page_url
+      @url
+    end
+
+    def last_page_url
+      @page_count != 1 ? "#{@url}#{@page_prefix}#{@page_count}" : @url
+    end
+
+    def previous_page_url
+      return unless @previous_page
+      @previous_page != 1 ? "#{@url}#{@page_prefix}#{@previous_page}" : @url
+    end
+
+    def next_page_url
+      "#{@url}#{@page_prefix}#{@next_page}" if @next_page
+    end
+
+    def to_h
+      {
+        url: @url,
+        current_page: @current_page,
+        page_count: @page_count,
+        item_count: @items.count,
+        previous_page: @previous_page,
+        next_page: @next_page,
+        links: {
+          current_page: current_page_url,
+          first_page: first_page_url,
+          last_page: last_page_url,
+          previous_page: previous_page_url,
+          next_page: next_page_url
+        }
+      }
+    end
+  end
+end
