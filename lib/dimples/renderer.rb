04def3d99c28cfb97bbec74fe263510103d6533d
Daniel Bogan <d@waferbaby.com>
2018-02-17 14:19:32 +1100

    Add the Renderer class

---
+++ lib/dimples/renderer.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Dimples
+  class Renderer
+    def initialize(site, source)
+      @site = site
+      @source = source
+    end
+
+    def render(context = {}, body = nil)
+      context[:site] ||= @site
+      output = engine.render(scope, context) { body }
+
+      return output unless (template = @site.templates[@source.metadata[:layout]])
+      template.render(context, output)
+    end
+
+    def engine
+      @engine ||= begin
+        callback = proc { @source.contents }
+
+        if @source.path
+          extension = File.extname(@source.path)
+          options = @site.config.rendering[extension.to_sym] || {}
+
+          Tilt.new(@source.path, options, &callback)
+        else
+          Tilt::StringTemplate.new(&callback)
+        end
+      end
+    end
+
+    def scope
+      @scope ||= Object.new.tap do |scope|
+        scope.instance_variable_set(:@site, @site)
+
+        scope.class.send(:define_method, :render) do |layout, locals = {}|
+          @site.templates[layout]&.render(locals)
+        end
+      end
+    end
+  end
+end
