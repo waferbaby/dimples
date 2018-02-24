1b550f0a1f3b6374335d81812148f43a90530cc4
Daniel Bogan <d@waferbaby.com>
2018-02-24 14:14:10 +1100

    Add filename and extension tests

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -2,6 +2,7 @@
 
 describe 'Page' do
   let(:site) { double }
+  let(:page) { Dimples::Page.new(site) }
 
   describe '#initialize' do
     context 'when a path is provided' do
@@ -28,12 +29,52 @@ PAGE_DATA
     end
 
     context 'when no path is provided' do
-      let(:page) { Dimples::Page.new(site) }
-
       it 'sets the default metadata and contents' do
         expect(page.contents).to eq('')
         expect(page.metadata).to eq({})
       end
     end
+  end
+
+  describe '#filename' do
+    context 'with no filename provided in the metadata' do
+      it 'returns the default filename' do
+        expect(page.filename).to eq('index')
+      end
+    end
+
+    context 'with a filename in the metadata' do
+      before do
+        page.metadata[:filename] = 'home'
+      end
+
+      it 'overrides the default value' do
+        expect(page.filename).to eq('home')
+      end
+    end
+  end
+
+  describe '#extension' do
+    context 'with no extension provided in the metadata' do
+      it 'returns the default extension' do
+        expect(page.extension).to eq('html')
+      end
+    end
+
+    context 'with an extension in the metadata' do
+      before do
+        page.metadata[:extension] = 'txt'
+      end
+
+      it 'overrides the default value' do
+        expect(page.extension).to eq('txt')
+      end
+    end
+  end
+
+  describe '#render' do
+  end
+
+  describe '#write' do
   end
 end
