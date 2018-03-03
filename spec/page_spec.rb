1efb54fdac993812e8f649725772405644eda519
Daniel Bogan <d@waferbaby.com>
2018-03-04 09:30:02 +1100

    Switch to using subject

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
 describe 'Page' do
+  subject { Dimples::Page.new(site, source_path) }
+
   let(:site) { double }
-  let(:page) { Dimples::Page.new(site, source_path) }
   let(:html) { '<p><em>Hey!</em></p>' }
   let(:source_path) { File.join(__dir__, 'sources', 'pages', 'index.markdown') }
 
   describe '#initialize' do
     context 'when a path is provided' do
       it 'parses the metadata and contents' do
-        expect(page.contents).to eq('*Hey!*')
-        expect(page.metadata).to eq(title: 'About', layout: false)
+        expect(subject.contents).to eq('*Hey!*')
+        expect(subject.metadata).to eq(title: 'About', layout: false)
       end
     end
 
     context 'when no path is provided' do
-      let(:page) { Dimples::Page.new(site) }
+      subject { Dimples::Page.new(site) }
 
       it 'sets the default metadata and contents' do
-        expect(page.contents).to eq('')
-        expect(page.metadata).to eq({})
+        expect(subject.contents).to eq('')
+        expect(subject.metadata).to eq({})
       end
     end
   end
@@ -27,17 +28,17 @@ describe 'Page' do
   describe '#filename' do
     context 'with no filename provided in the metadata' do
       it 'returns the default filename' do
-        expect(page.filename).to eq('index')
+        expect(subject.filename).to eq('index')
       end
     end
 
     context 'with a filename in the metadata' do
       before do
-        page.metadata[:filename] = 'home'
+        subject.metadata[:filename] = 'home'
       end
 
       it 'overrides the default value' do
-        expect(page.filename).to eq('home')
+        expect(subject.filename).to eq('home')
       end
     end
   end
@@ -45,17 +46,17 @@ describe 'Page' do
   describe '#extension' do
     context 'with no extension provided in the metadata' do
       it 'returns the default extension' do
-        expect(page.extension).to eq('html')
+        expect(subject.extension).to eq('html')
       end
     end
 
     context 'with an extension in the metadata' do
       before do
-        page.metadata[:extension] = 'txt'
+        subject.metadata[:extension] = 'txt'
       end
 
       it 'overrides the default value' do
-        expect(page.extension).to eq('txt')
+        expect(subject.extension).to eq('txt')
       end
     end
   end
@@ -68,15 +69,15 @@ describe 'Page' do
       end
 
       it 'renders the contents' do
-        expect(page.render).to eq(html)
+        expect(subject.render).to eq(html)
       end
     end
 
     context 'when the page has no path' do
-      let(:page) { Dimples::Page.new(site) }
+      subject { Dimples::Page.new(site) }
 
       it 'renders an empty string' do
-        expect(page.render).to eq('')
+        expect(subject.render).to eq('')
       end
     end
   end
@@ -85,14 +86,17 @@ describe 'Page' do
     let(:page_directory) { File.join(@site_output, 'pages') }
 
     before do
-      allow(page).to receive(:render).and_return(html)
+      allow(subject).to receive(:render).and_return(html)
     end
 
     context 'when we have correct permissions' do
       it 'writes out the file' do
-        page.write(page_directory)
+        subject.write(page_directory)
 
-        path = File.join(page_directory, "#{page.filename}.#{page.extension}")
+        path = File.join(
+          page_directory,
+          "#{subject.filename}.#{subject.extension}"
+        )
 
         expect(File.exist?(path)).to eq(true)
         expect(File.read(path)).to eq(html)
@@ -105,7 +109,7 @@ describe 'Page' do
       end
 
       it 'raises an exception' do
-        expect { page.write(page_directory) }.to raise_error(
+        expect { subject.write(page_directory) }.to raise_error(
           Dimples::PublishingError
         )
       end
@@ -115,15 +119,15 @@ describe 'Page' do
   describe '#inspect' do
     context 'when a path is provided' do
       it 'shows the correct string' do
-        expect(page.inspect).to eq("#<Dimples::Page @path=#{page.path}>")
+        expect(subject.inspect).to eq("#<Dimples::Page @path=#{subject.path}>")
       end
     end
 
     context 'when no path is provided' do
-      let(:page) { Dimples::Page.new(site) }
+      subject { Dimples::Page.new(site) }
 
       it 'shows the correct string' do
-        expect(page.inspect).to eq('#<Dimples::Page @path=>')
+        expect(subject.inspect).to eq('#<Dimples::Page @path=>')
       end
     end
   end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 describe 'Post' do
+  subject { Dimples::Post.new(site, source_path) }
+
   let(:site) { double }
-  let(:post) { Dimples::Post.new(site, source_path) }
   let(:source_path) do
     File.join(__dir__, 'sources', 'posts', '2018-01-01-hello.markdown')
   end
@@ -14,28 +15,28 @@ describe 'Post' do
 
   describe '#initialize' do
     it 'sets the post-specific metadata' do
-      expect(post.metadata[:date]).to eq(Date.new(2018, 1, 1))
-      expect(post.metadata[:slug]).to eq('hello')
-      expect(post.metadata[:layout]).to eq('post')
-      expect(post.metadata[:categories]).to eq(%w[personal dog])
+      expect(subject.metadata[:date]).to eq(Date.new(2018, 1, 1))
+      expect(subject.metadata[:slug]).to eq('hello')
+      expect(subject.metadata[:layout]).to eq('post')
+      expect(subject.metadata[:categories]).to eq(%w[personal dog])
     end
   end
 
   describe '#year' do
     it 'returns the correct value' do
-      expect(post.year).to eq('2018')
+      expect(subject.year).to eq('2018')
     end
   end
 
   describe '#month' do
     it 'returns the correct value' do
-      expect(post.month).to eq('01')
+      expect(subject.month).to eq('01')
     end
   end
 
   describe '#day' do
     it 'returns the correct value' do
-      expect(post.day).to eq('01')
+      expect(subject.day).to eq('01')
     end
   end
 end
