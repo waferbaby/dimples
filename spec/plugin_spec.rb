16977a891b00426ae8f9100b87d16fe88cb0f33c
Daniel Bogan <d@waferbaby.com>
2018-03-25 14:13:19 +1100

    Add plugin tests

---
+++ spec/plugin_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe 'Plugin' do
+  subject { Dimples::Plugin.new(site) }
+
+  let(:site) { double }
+
+  describe '.inherited' do
+    context 'when a subclass of Plugin is included' do
+      before do
+        class TestPluginA < Dimples::Plugin; end
+        class TestPluginB < Dimples::Plugin; end
+      end
+
+      it 'keeps a record of the subclass' do
+        subclasses = Dimples::Plugin.subclasses
+        expect(subclasses).to eq([TestPluginA, TestPluginB])
+      end
+    end
+  end
+
+  describe '#supports_event?' do
+    context 'when a plugin supports no events' do
+      it 'returns false for an event' do
+        expect(subject.supports_event?(:before_site_generation)).to eq(false)
+      end
+    end
+  end
+end
