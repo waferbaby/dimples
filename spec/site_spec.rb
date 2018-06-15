8cb7d71a7e4097de1d01dfcb4995f1e5eb499db1
Daniel Bogan <d@waferbaby.com>
2018-06-15 17:11:34 +1000

    Add the initial site tests

---
+++ spec/site_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+describe 'Site' do
+  subject { Dimples::Site.new(config) }
+
+  let(:config) { { source: File.join(__dir__, 'sources') } }
+
+  describe '#read_templates' do
+    before { subject.send(:read_templates) }
+
+    it 'finds all the templates' do
+      expect(subject.templates.count).to eq(3)
+      expect(subject.templates.keys.sort).to eq(
+        %w[default post shared.header].sort
+      )
+
+      subject.templates.each_value do |template|
+        expect(template).to be_a(Dimples::Template)
+      end
+    end
+  end
+
+  describe '#read_posts' do
+    before { subject.send(:read_posts) }
+
+    it 'finds all the posts' do
+      expect(subject.posts.count).to eq(2)
+
+      subject.posts.each do |page|
+        expect(page).to be_a(Dimples::Post)
+      end
+    end
+  end
+
+  describe '#read_pages' do
+    before { subject.send(:read_pages) }
+
+    it 'finds all the pages' do
+      expect(subject.pages.count).to eq(2)
+
+      subject.pages.each do |page|
+        expect(page).to be_a(Dimples::Page)
+      end
+    end
+  end
+end
