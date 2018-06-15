fe15f6d82d1fbc3b4667604a4ba901f04b36b84d
Daniel Bogan <d@waferbaby.com>
2018-06-15 17:54:40 +1000

    Simplify the frontable spec

--- spec/frontable_spec.rb
+++ spec/frontable_spec.rb
@@ -4,28 +4,15 @@ describe 'Frontable' do
   include Dimples::Frontable
 
   describe '#read_with_front_matter' do
-    context 'when a file has front matter' do
-      let(:source_path) do
-        File.join(__dir__, 'sources', 'pages', 'about', 'index.markdown')
-      end
-
-      it 'correctly parses the contents and metadata' do
-        contents, metadata = read_with_front_matter(source_path)
-
-        expect(contents).to eq('I am a test website.')
-        expect(metadata).to eq(title: 'About')
-      end
+    let(:source_path) do
+      File.join(__dir__, 'sources', 'pages', 'about', 'index.markdown')
     end
 
-    context 'when a file has no front matter' do
-      let(:source_path) { File.join(__dir__, 'sources', 'pages', 'info.txt') }
+    it 'correctly parses the contents and metadata' do
+      contents, metadata = read_with_front_matter(source_path)
 
-      it 'returns the contents without metadata' do
-        contents, metadata = read_with_front_matter(source_path)
-
-        expect(contents).to eq('I\'m just a text file.')
-        expect(metadata).to eq({})
-      end
+      expect(contents).to eq('I am a test website.')
+      expect(metadata).to eq(title: 'About')
     end
   end
 end
