5fc99da2bd6c8fb7f1b526c612738043ad65c176
Daniel Bogan <d@waferbaby.com>
2018-10-03 18:58:38 +1000

    Add a frontable test for files without metadata

--- spec/frontable_spec.rb
+++ spec/frontable_spec.rb
@@ -4,15 +4,32 @@ describe 'Frontable' do
   include Dimples::Frontable
 
   describe '#read_with_front_matter' do
-    let(:source_path) do
-      File.join(__dir__, 'sources', 'pages', 'about', 'index.markdown')
+    context 'with a file containing frontmatter' do
+      let(:source_path) do
+        File.join(__dir__, 'sources', 'pages', 'about', 'index.markdown')
+      end
+
+      it 'correctly parses the contents and metadata' do
+        contents, metadata = read_with_front_matter(source_path)
+
+        expect(contents).to eq('I am a test website.')
+        expect(metadata).to eq(title: 'About', layout: 'default')
+      end
     end
 
-    it 'correctly parses the contents and metadata' do
-      contents, metadata = read_with_front_matter(source_path)
+    context 'with a file containing no frontmatter' do
+      let(:source_path) do
+        File.join(__dir__, 'sources', 'templates', 'default.erb')
+      end
 
-      expect(contents).to eq('I am a test website.')
-      expect(metadata).to eq(title: 'About', layout: 'default')
+      let(:raw_content) { File.read(source_path) }
+
+      it 'reads in just the contents' do
+        contents, metadata = read_with_front_matter(source_path)
+
+        expect(contents).to eq(raw_content)
+        expect(metadata).to eq({})
+      end
     end
   end
 end
