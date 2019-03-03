1f512470920b6141632f955c175fb552bd180a04
Daniel Bogan <d@waferbaby.com>
2019-03-03 21:18:29 +1100

    Use a constant for the regexp pattern

--- lib/dimples/frontable.rb
+++ lib/dimples/frontable.rb
@@ -3,10 +3,12 @@
 module Dimples
   # Adds the ability to read frontmatter from a file.
   module Frontable
+    FRONT_MATTER_PATTERN = /^(-{3}\n.*?\n?)^(-{3}*$\n?)/m.freeze
+
     def read_with_front_matter(path)
       contents = File.read(path)
 
-      if (matches = contents.match(/^(-{3}\n.*?\n?)^(-{3}*$\n?)/m))
+      if (matches = contents.match(FRONT_MATTER_PATTERN))
         metadata = Hashie.symbolize_keys(YAML.safe_load(matches[1]))
         contents = matches.post_match.strip
       else
