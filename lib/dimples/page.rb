f5dc429bb7a91f929d31f10936cf989b3c1b0c91
Daniel Bogan <d@waferbaby.com>
2022-08-07 05:08:17 +0100

    Simply the class

--- lib/dimples/page.rb
+++ lib/dimples/page.rb
@@ -1,69 +1,34 @@
-# frozen_string_literal: true
+require 'date'
+require 'redcarpet'
+require 'yaml'
 
 module Dimples
-  # A single page on a site.
   class Page
-    attr_accessor :contents
-    attr_accessor :metadata
-    attr_accessor :path
+    FRONT_MATTER_PATTERN = /^(-{3}\n.*?\n?)^(-{3}*$\n?)/m.freeze
 
-    def initialize(site, path = nil)
-      @site = site
-      @path = path
+    attr_accessor :metadata, :contents
 
-      if @path
-        data = File.read(@path)
-        @contents, @metadata = FrontMatter.parse(data)
+    def initialize(path)
+      @contents = File.read(path)
+     
+      if matches = contents.match(FRONT_MATTER_PATTERN)
+        @metadata = YAML.load(matches[1])
+        @contents = matches.post_match.strip
       else
-        @contents = ''
         @metadata = {}
       end
     end
 
     def filename
+      "#{basename}.#{extension}"
+    end
+
+    def basename
       @metadata[:filename] || 'index'
     end
 
     def extension
       @metadata[:extension] || 'html'
-    end
-
-    def render(context = {})
-      metadata = @metadata.dup
-      metadata.merge!(context[:page]) if context[:page]
-
-      context[:page] = Hashie::Mash.new(metadata)
-
-      renderer.render(context)
-    end
-
-    def write(output_directory, context = {})
-      FileUtils.mkdir_p(output_directory) unless Dir.exist?(output_directory)
-      output_path = File.join(output_directory, "#{filename}.#{extension}")
-
-      File.write(output_path, render(context))
-    rescue SystemCallError => e
-      raise PublishingError, "Failed to publish file at #{output_path} (#{e})"
-    end
-
-    private
-
-    def renderer
-      @renderer ||= Renderer.new(@site, self)
-    end
-
-    def method_missing(method_name, *args, &block)
-      if @metadata.key?(method_name)
-        @metadata[method_name]
-      elsif (matches = method_name.match(/([a-z_]+)=/))
-        @metadata[matches[1].to_sym] = args[0]
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(method_name, include_private = false)
-      @metadata.key?(method_name) || method_name.match?(/([a-z_]+)=/) || super
     end
   end
 end
