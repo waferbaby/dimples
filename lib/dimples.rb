88f91e71cb83ef154b9a0b3129b84ee1bb9150dc
Daniel Bogan <d@waferbaby.com>
2022-08-17 08:38:25 +0000

    Simplify the requires

--- lib/dimples.rb
+++ lib/dimples.rb
@@ -2,22 +2,7 @@
 
 $LOAD_PATH.unshift(__dir__)
 
-require 'fileutils'
-require 'hashie'
-require 'tilt'
-require 'yaml'
-
-require 'dimples/archive'
-require 'dimples/category'
-require 'dimples/configuration'
-require 'dimples/errors'
-require 'dimples/frontmatter'
-require 'dimples/pager'
-require 'dimples/renderer'
-
 require 'dimples/page'
-require 'dimples/feed'
 require 'dimples/post'
 require 'dimples/template'
-
 require 'dimples/site'
