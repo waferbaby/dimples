b174a9dc4057692dab8f8c915fc333ca49d7a417
Daniel Bogan <d@waferbaby.com>
2022-08-18 03:24:27 +0000

    Add helper methods

--- lib/dimples/post.rb
+++ lib/dimples/post.rb
@@ -4,8 +4,24 @@ require 'date'
 
 module Dimples
   class Post < Document
+    def title
+      @metadata.fetch(:title, 'Untitled')
+    end
+
+    def summary
+      @metadata.fetch(:summary, '')
+    end
+
+    def categories
+      @metadata.fetch(:categories, [])
+    end
+
     def date
       @metadata.fetch(:date, DateTime.now)
+    end
+
+    def slug
+      File.basename(@path, '.markdown')
     end
   end
 end
