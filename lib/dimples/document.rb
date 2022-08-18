b681f48eb8ed81b06da95956ac31e1a858bb819c
Daniel Bogan <d@waferbaby.com>
2022-08-18 07:05:40 +0000

    Symbolise the frontmatter, rework the render method, add the method_missing handler

--- lib/dimples/document.rb
+++ lib/dimples/document.rb
@@ -4,14 +4,14 @@ module Dimples
   class Document
     FRONT_MATTER_PATTERN = /^(-{3}\n.*?\n?)^(-{3}*$\n?)/m.freeze
 
-    attr_accessor :metadata, :contents
+    attr_accessor :metadata, :contents, :path
 
     def initialize(path)
       @path = path
       @contents = File.read(path)
 
       if matches = contents.match(FRONT_MATTER_PATTERN)
-        @metadata = YAML.load(matches[1])
+        @metadata = YAML.load(matches[1], symbolize_names: true)
         @contents = matches.post_match.strip
       else
         @metadata = {}
@@ -31,18 +31,33 @@ module Dimples
     end
 
     def layout
-      @metadata.fetch('layout', nil)
+      @metadata.fetch(:layout, nil)
     end
 
-    def render(content = '', context = {})
-      renderer.render(Object.new, context) { content }
+    def render(context = {}, content = '')
+      context_obj = Object.new
+      context.each do |key, value|
+        context_obj.instance_variable_set("@#{key}", value)
+      end
+
+      renderer.render(context_obj) { content }
     end
+
+    private
 
     def renderer
       @renderer ||= begin
                       callback = proc { contents }
                       Tilt.new(@path, {}, &callback)
                     end
+    end
+
+    def method_missing(method_name, *args, &block)
+      if @metadata.key?(method_name)
+        @metadata[method_name]
+      else
+        nil
+      end
     end
   end
 end
