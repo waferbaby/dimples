4b3735109d02aa3fec18c5d5674185c8622692df
Daniel Bogan <d@waferbaby.com>
2022-08-18 03:24:08 +0000

    Store the path, and add the rendering methods

--- lib/dimples/document.rb
+++ lib/dimples/document.rb
@@ -7,8 +7,9 @@ module Dimples
     attr_accessor :metadata, :contents
 
     def initialize(path)
+      @path = path
       @contents = File.read(path)
-     
+
       if matches = contents.match(FRONT_MATTER_PATTERN)
         @metadata = YAML.load(matches[1])
         @contents = matches.post_match.strip
@@ -27,6 +28,21 @@ module Dimples
 
     def extension
       @metadata.fetch(:extension, 'html')
+    end
+
+    def layout
+      @metadata.fetch('layout', nil)
+    end
+
+    def render(content = '', context = {})
+      renderer.render(Object.new, context) { content }
+    end
+
+    def renderer
+      @renderer ||= begin
+                      callback = proc { contents }
+                      Tilt.new(@path, {}, &callback)
+                    end
     end
   end
 end
