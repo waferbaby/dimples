d1a999a1412dc4d956f05087b87df0d9fc039ba8
Daniel Bogan <d@waferbaby.com>
2022-08-20 23:08:56 +0000

    Add the FrontMatter module

---
+++ lib/dimples/frontmatter.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+module Dimples
+  module FrontMatter
+    PATTERN = /^(-{3}\n.*?\n?)^(-{3}*$\n?)/m.freeze
+
+    def self.parse(contents)
+      metadata = {}
+
+      if matches = contents.match(PATTERN)
+        metadata = YAML.load(matches[1], symbolize_names: true)
+        contents = matches.post_match.strip
+      end
+
+      [metadata, contents]
+    end
+  end
+end
