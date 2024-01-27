34afc7576004f74c0021183e0c6ff109e6c0b3ac
Daniel Bogan <d@waferbaby.com>
2024-01-27 08:46:07 +0000

    Refactor the post class

--- lib/dimples/post.rb
+++ lib/dimples/post.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
-require_relative 'document'
-
-require 'date'
-
 module Dimples
-  class Post < Document
+  # A page from a site with a date.
+  class Post < Page
     def date
-      @metadata.fetch(:date, DateTime.now)
+      @metadata.fetch(:date, File.birthtime(@path))
     end
 
     def layout
       @metadata.fetch(:layout, 'post')
     end
 
+    def categories
+      @metadata.fetch(:categories, [])
+    end
+
     def slug
-      File.basename(@path, '.markdown')
+      @metadata.fetch(:slug, File.basename(@path, '.markdown'))
+    end
+
+    def output_directory
+      @output_directory ||= File.dirname(@path).gsub(@config[:sources][:posts], @config[:output][:posts]) << "/#{slug}/"
+    end
+
+    def url
+      @url ||= output_directory.gsub(@config[:output][:root], '')
+    end
+
+    private
+
+    def template_class
+      Tilt::RedcarpetTemplate
+    end
+
+    def template_options
+      { smartypants: true }
     end
   end
 end
