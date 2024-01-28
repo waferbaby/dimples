7105fa8035089c3bb74bf3479263f8c106aba36f
Daniel Bogan <d@waferbaby.com>
2024-01-28 10:33:32 +0000

    Rework the whole shebang

--- lib/dimples/site.rb
+++ lib/dimples/site.rb
@@ -1,166 +1,135 @@
 # frozen_string_literal: true
 
-require_relative 'pager'
-
 require 'fileutils'
 require 'tilt'
 require 'date'
 
 module Dimples
-  # A
+  # A class representing a single generated website.
   class Site
-    DEFAULT_CONFIG = { generation: { overwrite_directory: false }, paths: { posts: 'posts' } }.freeze
+    attr_accessor :config
 
-    def self.generate(source_path, output_path, config)
-      new(source_path, output_path, config).generate
+    def self.generate(config = {})
+      new(config).generate
     end
 
-    attr_accessor :posts, :pages, :categories, :config
-
-    def initialize(source_path, output_path, config)
-      @paths = {
-        source: File.expand_path(source_path || Dir.pwd),
-        destination: File.expand_path(output_path || File.join(Dir.pwd, 'site'))
-      }
-
-      @config = DEFAULT_CONFIG
-      @config = @config.merge(config) if config.is_a?(Hash)
-
-      %w[pages posts static templates].each { |type| @paths[type.to_sym] = File.join(@paths[:source], type) }
-
-      scan_posts
-      scan_pages
-      scan_templates
+    def initialize(config = {})
+      @config = Config.new(config)
     end
 
     def generate
       prepare_output_directory
 
       generate_posts
-      generate_pages
       generate_categories
+      generate_pages
 
       copy_assets
     end
 
     private
 
-    def prepare_output_directory
-      if Dir.exist?(@paths[:destination])
-        unless @config.dig(:generation, :overwrite_directory)
-          raise GenerationError, "The site directory (#{@paths[:destination]}) already exists."
-        end
-
-        FileUtils.rm_rf(@paths[:destination])
-      end
-
-      Dir.mkdir(@paths[:destination])
+    def posts
+      @posts ||= Dir.glob(File.join(@config[:sources][:posts], '**', '*.markdown')).map do |path|
+        Dimples::Post.new(path, @config)
+      end.sort_by!(&:date).reverse!
     end
 
-    def read_files(path)
-      Dir[File.join(path, '**', '*.*')]
-    end
-
-    def scan_posts
-      @posts = read_files(@paths[:posts]).map { |path| Dimples::Post.new(path) }
-      @posts.sort_by!(&:date).reverse!
-
-      @categories = {}
-
-      @posts.each do |post|
-        post.categories&.each do |category|
-          @categories[category] ||= []
-          @categories[category] << post
-        end
+    def pages
+      @pages ||= Dir.glob(File.join(@config[:sources][:pages], '**', '*.erb')).map do |path|
+        Dimples::Page.new(path, @config)
       end
     end
 
-    def scan_pages
-      @pages = read_files(@paths[:pages]).map { |path| Dimples::Page.new(path) }
+    def layouts
+      @layouts ||= Dir.glob(File.join(@config[:sources][:layouts], '**', '*.erb')).to_h do |path|
+        [File.basename(path, '.erb'), Dimples::Layout.new(path, @config)]
+      end
     end
 
-    def scan_templates
-      @templates =
-        {}.tap do |templates|
-          read_files(@paths[:templates]).each do |path|
-            key = File.basename(path, '.erb')
-            templates[key] = Dimples::Template.new(path)
+    def categories
+      @categories ||= {}.tap do |categories|
+        posts.each do |post|
+          post.categories.each do |category|
+            categories[category] ||= []
+            categories[category].append(post)
           end
         end
-    end
-
-    def write_file(path, content)
-      directory_path = File.dirname(path)
-
-      FileUtils.mkdir_p(directory_path)
-      File.write(path, content)
-    end
-
-    def generate_paginated_posts(posts, path, context = {})
-      pager = Dimples::Pager.new("#{path.sub(@paths[:destination], '')}/", posts)
-
-      pager.each do |index|
-        page = Dimples::Page.new(nil, layout: 'posts')
-        page_path = index == 1 ? path : File.join(path, "page_#{index}")
-
-        write_file(
-          File.join(page_path, page.filename),
-          render(page, context.merge!(pagination: pager.to_context))
-        )
       end
     end
 
     def generate_posts
-      directory_path = File.join(@paths[:destination], @config.dig(:paths, :posts))
-      Dir.mkdir(directory_path)
-
-      @posts.each do |post|
-        path = File.join(directory_path, post.slug, post.filename)
-        write_file(path, render(post, post: post))
+      posts.each do |post|
+        write_item(post)
       end
 
-      generate_paginated_posts(@posts, directory_path)
-      generate_feed(@posts.slice(0, 10), @paths[:destination])
-    end
-
-    def generate_pages
-      @pages.each do |page|
-        path =
-          if page.path
-            File.dirname(page.path).sub(@paths[:pages], @paths[:destination])
-          else
-            @paths[:destination]
-          end
-
-        write_file(File.join(path, page.filename), render(page, page: page))
+      Pager.paginate('/interviews/', posts, @config) do |pagination|
+        path = File.join(@config[:output][:root], pagination[:urls][:current_page])
+        write_item(layouts['posts'], output_path: path, metadata: { pagination: pagination })
       end
     end
 
     def generate_categories
-      @categories.each do |category, posts|
-        category_path = File.join(@paths[:destination], 'categories', category)
+      categories.each do |category, posts|
+        Pager.paginate("/categories/#{category}/", posts, @config) do |pagination|
+          path = File.join(@config[:output][:root], pagination[:urls][:current_page])
+          metadata = { title: category.capitalize, category: category, pagination: pagination }
 
-        generate_paginated_posts(posts, category_path, category: category)
-        generate_feed(posts.slice(0, 10), category_path)
+          write_item(layouts['posts'], output_path: path, metadata: metadata)
+        end
       end
     end
 
-    def generate_feed(posts, path)
-      page = Dimples::Page.new(nil, layout: 'feed')
-      write_file(File.join(path, 'feed.atom'), render(page, posts: posts))
+    def generate_pages
+      pages.each do |page|
+        write_item(page)
+      end
+    end
+
+    def write_item(item, output_path: nil, metadata: {})
+      output_path ||= item.output_directory
+
+      FileUtils.mkdir_p(output_path) unless File.directory?(output_path)
+
+      contents = render_item(item, metadata: metadata)
+      File.write(File.join(output_path, item.filename), contents)
+    end
+
+    def render_item(item, body: nil, metadata: {})
+      metadata = item.metadata.merge(metadata)
+      output = item.template.render(metadata_context(metadata)) { body }
+      return output unless item.layout && layouts[item.layout]
+
+      render_item(layouts[item.layout], body: output, metadata: metadata)
+    rescue StandardError => e
+      raise "Failed to render #{item.path}: #{e}"
+    end
+
+    def metadata_context(metadata)
+      Object.new.tap do |context|
+        shared_metadata.merge(metadata).each do |key, value|
+          context.instance_variable_set("@#{key}", value)
+        end
+      end
+    end
+
+    def shared_metadata
+      @shared_metadata ||= {
+        posts: posts,
+        categories: categories
+      }
+    end
+
+    def prepare_output_directory
+      raise "The site directory (#{@config[:output][:root]}) already exists." if Dir.exist?(@config[:output][:root])
+
+      Dir.mkdir(@config[:output][:root])
     end
 
     def copy_assets
-      return unless Dir.exist?(@paths[:static])
+      return unless Dir.exist?(@config[:sources][:static])
 
-      FileUtils.cp_r(File.join(@paths[:static], '.'), @paths[:destination])
-    end
-
-    def render(object, context = {}, content = nil)
-      context[:site] ||= self
-
-      output = object.render(context, content)
-      render(@templates[object.layout], context, output) if object.layout && @templates[object.layout]
+      FileUtils.cp_r(File.join(@config[:sources][:static], '.'), @config[:output][:root])
     end
   end
 end
