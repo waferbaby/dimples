5175bcd39f7ac68787eb4b5ea13283013fde85e0
Daniel Bogan <d@waferbaby.com>
2025-11-16 15:12:29 +1100

    Add the initial site spec

---
+++ spec/dimples/site_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'tmpdir'
+
+describe Dimples::Site do
+  subject(:site) { described_class.new(config:) }
+
+  let(:config) { {
+    source: File.join(File.dirname(__dir__), 'fixtures'),
+    build: File.join(Dir.tmpdir, "dimples-test-site-#{Time.now.to_i}")
+  } }
+
+  describe '#posts' do
+    it 'builds a list of post objects' do
+      expect(site.posts.count).to eql(2)
+    end
+  end
+
+  describe '#pages' do
+    it 'builds a list of page objects' do
+      expect(site.pages.count).to eql(1)
+    end
+  end
+
+  describe '#layouts' do
+    it 'builds a list of layout objects' do
+    end
+  end
+
+  describe '#categories' do
+    it 'builds a list of posts grouped by category' do
+      expect(site.categories.keys).to eql(%w[meta test personal])
+    end
+  end
+
+  describe '#prepare_output_directory' do
+    before { FileUtils.rm_rf(site.config.build_paths[:root]) }
+
+    context 'when the target directory does not exist' do
+      it 'creates the directory' do
+        site.send(:prepare_output_directory)
+        expect(Dir.exist?(site.config.build_paths[:root])).to eql(true)
+      end
+    end
+
+    context 'when the target directory already exists' do
+      before { Dir.mkdir(site.config.build_paths[:root]) }
+
+      it 'raises an error' do
+        expect { site.send(:prepare_output_directory) }.to raise_error(RuntimeError)
+      end
+    end
+  end
+end
