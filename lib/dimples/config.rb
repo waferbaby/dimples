65461a61058e4ad0c32c4775c6b2eaf4fa339015
Daniel Bogan <d@waferbaby.com>
2025-11-25 18:51:10 +1100

    Add the site details and feed generation

--- lib/dimples/config.rb
+++ lib/dimples/config.rb
@@ -5,15 +5,16 @@ module Dimples
   class Config
     SOURCE_PATHS = { pages: 'pages', posts: 'posts', layouts: 'layouts', static: 'static' }.freeze
 
-    attr_accessor :source_paths, :build_paths, :pagination, :generation
+    attr_accessor :source_paths, :build_paths, :site, :pagination, :generation
 
     def self.defaults
       {
-        build: './site',
         source: Dir.pwd,
+        build: './site',
         pathnames: { posts: 'posts', categories: 'categories' },
+        site: { name: nil, domain: nil },
         pagination: { page_prefix: 'page_', per_page: 5 },
-        generation: { sitemap: false, api: false }
+        generation: { api: false, main_feed: true, category_feeds: false }
       }
     end
 
@@ -22,6 +23,7 @@ module Dimples
 
       @source_paths = expand_paths(File.expand_path(options[:source]), SOURCE_PATHS.dup)
       @build_paths = expand_paths(File.expand_path(options[:build]), options[:pathnames])
+      @site = options[:site]
       @pagination = options[:pagination]
       @generation = options[:generation]
     end
