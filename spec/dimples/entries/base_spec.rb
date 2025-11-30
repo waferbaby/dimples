d0f74e995ad4446dc794a65e6de5e4f2eca087f2
Daniel Bogan <d@waferbaby.com>
2025-11-30 20:10:21 +1100

    Flesh out the specs

--- spec/dimples/entries/base_spec.rb
+++ spec/dimples/entries/base_spec.rb
@@ -1,19 +1,55 @@
 # frozen_string_literal: true
 
 describe Dimples::Entries::Base do
-  subject(:entry) { described_class.new(site: site, contents: contents) }
+  subject(:base) { described_class.new(site: site, contents: contents) }
 
   let(:site) { double }
-  let(:contents) { 'I am content!' }
+  let(:contents) { "HEY!\n\nI am content!" }
 
   describe '#parse_metadata' do
     it 'correctly parses the contents' do
-      expect(entry.metadata).to eql({ filename: 'index.html', layout: nil })
+      expect(base.metadata).to eql({ filename: 'index.html', layout: nil })
+    end
+  end
+
+  describe '#write' do
+    let(:path) { File.join(Dir.tmpdir, 'dimples-test-file') }
+
+    before do
+      allow(site).to receive(:metadata).and_return({})
+
+      base.write(output_path: path)
     end
 
-    it 'adds the metadata methods' do
-      entry.metadata.each_key do |method_name|
-        expect(entry).to respond_to(method_name)
+    it 'create the file at the correct location' do
+      expect(File.exist?(path)).to be(true)
+    end
+
+    it 'writes the correct file contents' do
+      expect(File.read(path)).to eql(contents)
+    end
+  end
+
+  describe '#method_missing' do
+    context 'with a key that matches a metadata key' do
+      let(:contents) do
+        <<~METADATA
+          ---
+          sporks: 12
+          ---
+
+          I love sporks!
+        METADATA
+      end
+
+      it 'returns the corresponding value' do
+        expect(base.sporks).to be(12)
+      end
+    end
+
+    context 'with a key that is not in the metadata' do
+      it 'returns a nil value' do
+        expect(base.spoons).to be_nil
       end
     end
   end
